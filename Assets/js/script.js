1f157f737734a95401fd497005ac8e512e480aca
fac-73 <kaydavis21@gmail.com>
2021-02-04 14:40:18 -0800

    updated css styles for list item, moved text from html to js file for initial load content

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -19,7 +19,7 @@ function rendercityHistory() {
     var cities = cityHistory[i];
 
     // creates the rows and content for city history list, sets text to the city name
-    var li = document.createElement("li");   
+    var li = document.createElement("li");
     li.textContent = cities;
 
     // adds it to the HTML document
@@ -34,7 +34,7 @@ function init() {
   if (storedcityHistory !== null) {
     cityHistory = storedcityHistory;
   }
-
+  // Run function if there are items in localStorage
   rendercityHistory();
 }
 
@@ -183,6 +183,8 @@ $(document).ready(function () {
   $("#wind").hide();
   $("#humidity").hide();
   $("#UV").hide();
+  // Set text on load
+  $("#currentCity").text("There is no such thing as bad weather, only different kinds of good weather.");
   $("#temp").text(" - John Ruskin");
 
   // event listener on submission of form - enter or click
@@ -194,7 +196,7 @@ $(document).ready(function () {
     // console.log(city)
     getWeather(cityFromInput);
     getForecast(cityFromInput);
-    
+
     var citiesSearched = inputCity.value.trim();
     if (citiesSearched === "") {
       return;
@@ -213,10 +215,10 @@ $(document).ready(function () {
   init()
 
   // Event handler for searching from search history list
-  $("li").on("click", function() {
+  $("li").on("click", function () {
     getWeather($(this).text());
     // Add pointer style on hover to show its a clickable item
-    $("li").css( 'cursor', 'pointer' );
+    $("li").css('cursor', 'pointer');
   });
-  
+
 });
